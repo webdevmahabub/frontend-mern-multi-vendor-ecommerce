47d640da90c9cb4a08c0609dce91b4f63ee2a626
webdevmahabub <mahbubbd926@gmail.com>
2024-10-18 12:41:45 +0600

    Banner Design Tailwind Css React Part 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from '../components/Header';
+import Banner from '../components/Banner';
 const Home = () => {
     return (
         <div className='w-full'>
             <Header/>
+            <Banner/>
         </div>
     );
 };
