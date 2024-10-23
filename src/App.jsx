c4a6573442dbb47778d4b289e4fc87074e506910
webdevmahabub <mahbubbd926@gmail.com>
2024-10-23 19:24:30 +0600

    Product Details Page Design Part 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Shops from './pages/Shops';
 import Cart from './pages/Cart';
 import Shipping from './pages/Shipping';
+import Details from './pages/Details';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path='/shops' element={<Shops/>} />
       <Route path='/cart' element={<Cart/>} />
       <Route path='/shipping' element={<Shipping/>} />
+      <Route path='/product/details/:slug' element={<Details/>} /> 
     </Routes>
     
     </BrowserRouter>
