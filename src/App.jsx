6923b1b2da191e44d906e905bcc2cb3f6257fab4
webdevmahabub <mahbubbd926@gmail.com>
2024-10-26 15:08:42 +0600

    Customer Login Register Page Design Part 1

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,16 @@ import Shops from './pages/Shops';
 import Cart from './pages/Cart';
 import Shipping from './pages/Shipping';
 import Details from './pages/Details';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>} />
+      <Route path='/login' element={<Login/>} />
+      <Route path='/register' element={<Register/>} />
       <Route path='/shops' element={<Shops/>} />
       <Route path='/cart' element={<Cart/>} />
       <Route path='/shipping' element={<Shipping/>} />
