f9bc1110fc1faeb2f8b3ccc8d360ac44f495f5b7
webdevmahabub <mahbubbd926@gmail.com>
2024-10-27 01:09:34 +0600

    Fetch Feature Product and Latest Top Rated Discount Part 1

--- src/store/reducers/homeReducer.js
+++ src/store/reducers/homeReducer.js
@@ -14,6 +14,19 @@ export const get_category = createAsyncThunk(
 )
 // End Method 
 
+export const get_products = createAsyncThunk(
+    'product/get_products',
+    async(_, { fulfillWithValue }) => {
+        try {
+            const {data} = await api.get('/home/get-products')
+             console.log(data)
+            return fulfillWithValue(data)
+        } catch (error) {
+            console.log(error.respone)
+        }
+    }
+)
+// End Method 
 
 export const homeReducer = createSlice({
     name: 'home',
