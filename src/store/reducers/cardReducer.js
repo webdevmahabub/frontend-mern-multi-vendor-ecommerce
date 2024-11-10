793de6fddd9adda36d1fda1db0b1a48480b21d6a
webdevmahabub <mahbubbd926@gmail.com>
2024-11-10 22:10:36 +0600

    Product Add to Card Part 4

--- src/store/reducers/cardReducer.js
+++ src/store/reducers/cardReducer.js
@@ -34,8 +34,14 @@ export const cardReducer = createSlice({
  
     },
     extraReducers: (builder) => {
-        
-        
+        builder
+        .addCase(add_to_card.rejected, (state, { payload }) => {
+            state.errorMessage = payload.error; 
+        })
+        .addCase(add_to_card.fulfilled, (state, { payload }) => { 
+            state.successMessage = payload.message; 
+            state.card_product_count = state.card_product_count + 1
+        })
     }
 })
 export const {messageClear} = cardReducer.actions
