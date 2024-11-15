8fdfa47bb062372c975924768aa5eff068bdf88d
webdevmahabub <mahbubbd926@gmail.com>
2024-11-15 17:23:54 +0600

    Customer Order Place Setup

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import { get_category } from './store/reducers/homeReducer';
 import { useDispatch} from 'react-redux';
 import CategoryShop from './pages/CategoryShop';
 import SearchProducts from './pages/SearchProducts';
+import Payment from './pages/Payment';
 
 function App() {
   const dispatch = useDispatch()
@@ -28,6 +29,7 @@ function App() {
       <Route path='/shops' element={<Shops/>} />
       <Route path='/card' element={<Card/>} />
       <Route path='/shipping' element={<Shipping/>} />
+      <Route path='/payment' element={<Payment/>} />
       <Route path='/product/details/:slug' element={<Details/>} /> 
       <Route path='/products?' element={<CategoryShop/>} />
       <Route path='/products/search?' element={<SearchProducts/>} />
--- src/pages/Shipping.jsx
+++ src/pages/Shipping.jsx
@@ -42,7 +42,7 @@ const Shipping = () => {
             shipping_fee,
             items,
             shippingInfo : state,
-            userId: userInfo,
+            userId: userInfo.id,
             navigate 
         }))
     }
