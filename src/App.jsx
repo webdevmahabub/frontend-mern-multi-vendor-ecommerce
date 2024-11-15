156af3bd99bb652b9539e15bdae21691d577454e
webdevmahabub <mahbubbd926@gmail.com>
2024-11-15 22:48:23 +0600

    Customer Order Details Get and Show Part 1

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ import Index from './components/dashboard/Index';
 import Orders from './components/dashboard/Orders';
 import ChangePassword from './components/dashboard/ChangePassword';
 import Wishlist from './components/dashboard/Wishlist';
+import OrderDetails from './components/dashboard/OrderDetails';
 function App() {
   const dispatch = useDispatch()
   useEffect(() => {
@@ -45,6 +46,7 @@ function App() {
       <Route path='my-orders' element={<Orders/>} /> 
       <Route path='change-password' element={<ChangePassword/>} />  
       <Route path='my-wishlist' element={<Wishlist/>} />
+      <Route path='order/details/:orderId' element={<OrderDetails/>} /> 
 
        </Route> 
       </Route>
