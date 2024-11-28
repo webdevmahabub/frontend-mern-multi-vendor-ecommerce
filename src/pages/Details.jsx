b6ee2a2eada37a1e842c48d9ff93dc4be8f6ca7e
webdevmahabub <mahbubbd926@gmail.com>
2024-11-28 18:58:59 +0600

    Update Product By Shop Name in Details Page

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -241,6 +241,7 @@ const Details = () => {
 
           <div className='text-slate-600'>
           <p>{product.description}  </p>
+          <p className='text-slate-600 py-1 font-bold'>Shop Name : {product.shopName}</p>
            </div> 
 
             <div className='flex gap-3 pb-10 border-b'>
@@ -328,7 +329,7 @@ const Details = () => {
 <div className='w-[28%] md-lg:w-full'>
 <div className='pl-4 md-lg:pl-0'>
     <div className='px-3 py-2 text-slate-600 bg-slate-200'>
-        <h2 className='font-bold'>From Green Shop</h2>
+    <h2 className='font-bold'>From {product.shopName}</h2>
     </div>
     <div className='flex flex-col gap-5 mt-3 border p-3'>
         {
