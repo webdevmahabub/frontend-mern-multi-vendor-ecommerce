734df528b506407261d6473a9744eeb5abb2246e
webdevmahabub <mahbubbd926@gmail.com>
2024-11-28 19:50:52 +0600

    Customer to Seller Chat System Part 1

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -3,12 +3,13 @@ import authReducer from "./reducers/authReducer";
 import cardReducer from "./reducers/cardReducer";
 import orderReducer from "./reducers/orderReducer";
 import dashboardReducer from "./reducers/dashboardReducer";
-
+import chatReducer from "./reducers/chatReducer";
 const rootReducer = {
     home: homeReducer,
     auth: authReducer,
     card: cardReducer,
     order: orderReducer,
-    dashboard: dashboardReducer
+    dashboard: dashboardReducer,
+    chat: chatReducer
 }
 export default rootReducer;
